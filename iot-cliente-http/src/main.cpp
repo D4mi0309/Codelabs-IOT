452cc667dad0ad686b3bf016137620558aab6cb3
D4mi0309 <damian.gil01@uceva.edu.co>
2025-03-14 16:26:17 -0500

    Se agregaron los archivos del ejercicio cliente http

---
+++ iot-cliente-http/src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include "libWifi.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define LED 2
+
+const char* ssid = "DAMIAN";
+const char* pass = "03102003";
+const char* thingspeak_url = "http://api.thingspeak.com/update";
+const char* api_key = "5J4KW6BS29MGCUYB";
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Iniciando Cliente HTTP");
+  conectarWifi(ssid, pass);
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    float temperatura = random(25, 30) + random(0, 100) / 100.0; //Grados Celcius
+    float humedad = random(40, 60) + random(0, 100) / 100.0;
+
+    String url = String(thingspeak_url) + "?api_key=" + api_key + "&field1=" + String(temperatura) + "&field2=" + String(humedad);
+
+    http.begin(url);
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      String payload = http.getString();
+      Serial.println("Respuesta: " + payload);
+    } else {
+      Serial.println("Error en la petición: " + String(httpCode));
+    }
+
+    http.end();
+  } else {
+    Serial.println("Conexión WiFi perdida");
+  }
+
+  delay(10000); // Diez segundos
+}
